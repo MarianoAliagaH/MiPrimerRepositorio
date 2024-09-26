5bae2a533464e72eb4070f3850a8a935052e01d7
MarianoAliagaH <n00323456@upn.pe>
2024-09-26 12:17:08 -0500

    Mande mi primer archivo README

---
+++ Form1.cs
@@ -0,0 +1,15 @@
+namespace ProyectoEjemplo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSaludar_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hola, Mundo!");
+        }
+    }
+}
